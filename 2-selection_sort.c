1522279f947742729076db731035415b4d4246d4
FouedDadi <foued.dadii@gmail.com>
2020-06-18 11:50:09 +0100

    2

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ *selection_sort - function that sorts an array of integers in ascending order
+ *@size: size of the array
+ *@array: the array that will be sorted
+ */
+void selection_sort(int *array, size_t size)
+{
+unsigned int i, j, min;
+int tmp;
+for (i = 0; i < size - 1; i++)
+{
+    min = i;
+    for (j = i + 1; j < size; j++)
+    {
+        if (array[j] < array[min])
+        {
+            min = j;
+        }
+    }
+    tmp = array[i];
+    array[i] = array[min];
+    array[min] = tmp;
+    if (min != i)
+        print_array(array, size);
+}
+}
